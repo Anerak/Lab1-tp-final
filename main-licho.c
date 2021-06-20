4451ae5ec88c86e63a850a528e5b5b6d764fb36b
lich03 <lichobidegain@gmail.com>
2021-06-20 00:37:15 -0300

    tomar orden,buscar producto,mostrar producto

--- main-licho.c
+++ main-licho.c
@@ -1,9 +1,44 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define CANT_MESA 10
-
+#define PRODUCTOS_LIMITE 50
 typedef enum {Mesero, Cajero, Manager, Owner} Rol;
+
+typedef enum
+{
+    Entrada,
+    Plato_principal,
+    Postre,
+    Bebida
+} ComidaTipo;
+
+typedef enum
+{
+    Pastas = 100,
+    Minutas,
+    Pizzas,
+    Empanadas,
+    Gaseosas = 200,
+    Aguas,
+    Vinos,
+    Tragos,
+    Helados = 300,
+    Flan,
+    Cupcakes
+} ComidaSubcategoria;
+
+typedef struct
+{
+    int id;
+    int activo;
+    char nombre[40];
+    char detalles[40];
+    float precio;
+    ComidaTipo comida_tipo;
+    ComidaSubcategoria subCategoria;
+} Comida;
 
 typedef struct {
     int id;
@@ -12,18 +47,6 @@ typedef struct {
     char apellido[20];
     Rol rol;
 } Empleado;
-
-typedef enum {Entrada, Plato_principal, Minutas, Postre, Bebida} ComidaTipo;
-//typedef enum {Minutas, Plato_principal, Minutas, Postre, Bebida} SubCategoria;
-
-typedef struct {
-    int id;
-    int activo;
-    char nombre[30];
-    float precio;
-    ComidaTipo comida_tipo;
-    //SubCategoria subCategoria = 0;
-} Comida;
 
 typedef struct {
     int id;
@@ -46,11 +69,20 @@ void inicMesas (Mesa mesas[]);
 int buscarMesa (Mesa mesas[],int id);
 void desocuparMesa (Mesa *mesa);
 void mostrarMesas (Mesa mesas[]);
+int productosPrueba(Comida productos[]);
+void agregarProducto(Comida *producto, char nombre[40], char detalles[40], float precio, int id, int comidaTipo, int comidaSubcategoria);
+void mostrarProducto(Comida producto);
+int guardarProductos(Comida productos[PRODUCTOS_LIMITE]);
+int buscarPosProducto(Comida productos[PRODUCTOS_LIMITE], int size, int id);
+void ordenMesa (Comida productos[],int size,Mesa *mesa);
+void mostrarPedido(Pedido pedido);
 
 int main ()
 {
     int idMesa=0;
     char nuevaMesa = 's';
+    Comida productos[PRODUCTOS_LIMITE];
+    int productosCargados = productosPrueba(productos);
     Mesa mesas[CANT_MESA];
     inicMesas(mesas);
     mostrarMesas(mesas);
@@ -67,9 +99,13 @@ int main ()
         if (posMesa >-1)
         {
         ocuparMesa (&mesas[posMesa]);
+        ordenMesa(productos,productosCargados,&mesas[posMesa]);
+        mostrarPedido(mesas[posMesa].pedidos[0]);
         }
     }
     mostrarMesas(mesas);
+
+ return 0;
 }
 //funcion para ocupar mesa, para tomar orden de la mesa.
 
@@ -89,6 +125,7 @@ void inicMesas (Mesa mesas[])
         mesas[i].ocupada=0;
         mesas[i].id= i+1;
         mesas[i].activo=1;
+        mesas[i].cantOrd=0;
 
     }
 }
@@ -128,5 +165,159 @@ void mostrarMesas (Mesa mesas[])
         }
     }
     printf("\n");
+
+}
+int productosPrueba(Comida productos[])
+{
+    char *nombres[] = {
+        "Sorrentinos",
+        "Canelones",
+        "Fideos",
+        "Milanesa",
+        "Hamburguesa",
+        "Pechuga Grille",
+        "Pizza",
+        "Pizza",
+        "Pizza",
+        "Empanadas",
+        "Empanadas",
+        "Empanadas",
+        "Empanadas",
+        "Empanadas",
+        "Coca Cola",
+        "Sprite",
+        "Fanta",
+        "Agua",
+        "Agua",
+        "Gatorade",
+        "Vasco Viejo 3/4",
+        "Vasco Viejo 3/4",
+        "Santa Julia 3/4",
+        "Fernet",
+        "Gin Tonic",
+        "Caipirinha",
+        "Caipiroska", "Almendrado", "Tricolor", "Bombon", "Bombon", "Bocha de helado", "Bocha de helado", "Flan", "Flan", "Flan", "Chocotorta", "Lemon Pie"};
+    char *detalles[] = {
+        "con bolognesa", "con filetto", "con salsa blanca", "a caballo", "completa", "con ensalada", "Napolitana", "Especial", "Extra Gulyx", "Carne", "Pollo", "Jamon y Queso", "Capresse", "Humita", "", "", "", "Saborizada", "c/s gas", "", "Tinto", "Blanco", "Blanco", "con Coca Cola", "", "", "", "", "", "Suizo", "Escoces", "Chocolate", "Vainilla", "solo", "con dulce de leche o crema", "mixto", "", ""};
+    float precios[] = {
+        260.0, 230.0, 200.0, 250.0, 280.0, 300.0, 500.0, 500.0, 500.0, 70.0, 70.0, 70.0, 70.0, 70.0, 120.0, 120.0, 120.0, 140.0, 100.0, 180.0, 260.0, 260.0, 350.0, 300.0, 300.0, 340.0, 340.0, 160.0, 160.0, 180.0, 180.0, 100.0, 100.0, 140.0, 170.0, 190.0, 300.0, 280.0};
+    ComidaTipo comidaTipo[] = {
+        Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Plato_principal, Bebida, Bebida, Bebida, Bebida, Bebida, Bebida, Bebida, Bebida, Bebida, Bebida, Bebida, Bebida, Bebida, Postre, Postre, Postre, Postre, Postre, Postre, Postre, Postre, Postre, Postre, Postre};
+    ComidaSubcategoria comidaSubcategoria[] = {
+        Pastas, Pastas, Pastas, Minutas, Minutas, Minutas, Pizzas, Pizzas, Pizzas, Empanadas, Empanadas, Empanadas, Empanadas, Empanadas, Gaseosas, Gaseosas, Gaseosas, Aguas, Aguas, Aguas, Vinos, Vinos, Vinos, Tragos, Tragos, Tragos, Tragos, Helados, Helados, Helados, Helados, Helados, Helados, Flan, Flan, Flan, Cupcakes, Cupcakes};
+
+    int size = sizeof(nombres) / sizeof(nombres[0]);
+    for (int i = 0; i < size; i++)
+    {
+        if (nombres[i] == NULL && detalles[i] == NULL)
+        {
+            break;
+        }
+        agregarProducto(&productos[i], nombres[i], detalles[i], precios[i], i, comidaTipo[i], comidaSubcategoria[i]);
+    }
+   
+    printf("\nProductos cargados\n");
+    return 1;
+}
+void agregarProducto(Comida *producto, char nombre[40], char detalles[40], float precio, int id, int comidaTipo, int comidaSubcategoria)
+{
+    producto->activo = 1;
+    producto->id = id;
+    producto->precio = precio;
+
+    strcpy(producto->nombre, nombre);
+    strcpy(producto->detalles, detalles);
+
+    producto->comida_tipo = comidaTipo;
+    producto->subCategoria = comidaSubcategoria;
+
+}
+void mostrarProducto(Comida producto)
+{
+    printf("\nID: %d\n", producto.id);
+    printf("\nNombre: %s\n", producto.nombre);
+    printf("Detalles: %s\n", producto.detalles);
+    printf("Precio: %.2f\n", producto.precio);
+    printf("Categoria: %d\n", producto.comida_tipo);
+    printf("Subcategoria: %d\n", producto.subCategoria);
+    printf("\n");
+}
+int guardarProductos(Comida productos[PRODUCTOS_LIMITE])
+{
+    FILE *fProductos;
+    char *ubicacion = "Productos.dat";
+
+    fProductos = fopen(ubicacion, "wb");
+    if (fProductos != NULL)
+    {
+        fwrite(&productos, sizeof(Comida), 1, fProductos);
+        return 1;
+    }
+    fclose(fProductos);
+    return 0;
+}
+void ordenMesa (Comida productos[],int size,Mesa *mesa)
+{
+    Pedido temp;
+    temp.cantItems=0;
+    int categoria=0,idProducto=0;
+    int posProducto =0;
+    char seguir = 's';
+    
+    printf("Elija la categoria 1 2 3\n");
+    scanf("%d",&categoria);
+
+    mesa->pedidos[mesa->cantOrd] = temp;      
+    temp.id = mesa->cantOrd+1;
+
+    do{
+    switch (categoria)
+    {
+    case 1:
+        printf("Comidas\n");
+        scanf("%d",&idProducto);
+        posProducto=buscarPosProducto(productos,size,idProducto);
+        //mesa->pedidos[mesa->cantOrd]=productos[posProducto];
+        temp.items[temp.cantItems] = productos[posProducto];
+        temp.cantItems++;
+    break;
+    case 2:
+        printf("Bebidas\n");
+    break;
+    case 3:
+        printf("Postres\n");
+    break;    
+
+    default:
+        break;
+    }
+     printf("Desea continuar ingresando pedidos? S/N \n");
+     fflush(stdin);
+     scanf("%c",&seguir);                                                                                                                                                                                                                                                       
+    }while (seguir =='s');
+    mesa->cantOrd++;
+
+}
+int buscarPosProducto(Comida productos[PRODUCTOS_LIMITE], int size, int id)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (productos[i].id == id)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+void mostrarPedido (Pedido pedido)
+{
+    float suma = 0;
+    printf("Pedido %d \n",pedido.id);
+    for (int i = 0; i < pedido.cantItems; i++)
+    {
+        suma += pedido.items[i].precio;
+        printf("%s %s - %.2f\n",pedido.items[i].nombre,pedido.items[i].detalles,pedido.items[i].precio);
+    }
+    printf("Total: %.2f",suma); 
 
 }
