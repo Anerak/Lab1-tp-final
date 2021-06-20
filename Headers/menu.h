617c7a0fde365ab2ac98398bac0ca889b38f73df
Guntergs <santii001@hotmail.com>
2021-06-20 20:21:53 -0300

    falto menu.h

--- Headers/menu.h
+++ Headers/menu.h
@@ -1,3 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
+
+void mostrarLogo();
+void ingPostres();
+void ingBebidas();
+void ingComidas();
+void ingProductosMesa();
+void menuMesa();
+void manejoMesas();
+void ingEliEmpleado();
+void inicioGerente();
+void inicioDuenio();
+void mostrarCajero();
+void mostrarGerente();
+void mostrarDuenio();
+void mostrarMenu();
