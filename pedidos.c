3bbc0e9ed7ea634092970677dffce26363cda9b5
Anerak <anerakco@gmail.com>
2021-06-20 21:43:10 -0300

    Update pedidos.c

--- pedidos.c
+++ pedidos.c
@@ -1,1 +1,13 @@
 #include "Headers\\pedidos.h"
+
+void mostrarPedido(Pedido pedido)
+{
+    float suma = 0;
+    printf("\nPedido %d \n", pedido.id);
+    for (int i = 0; i < pedido.cantItems; i++)
+    {
+        suma += pedido.items[i].precio;
+        printf("%s %s - %.2f\n", pedido.items[i].nombre, pedido.items[i].detalles, pedido.items[i].precio);
+    }
+    printf("Total: %.2f\n", suma);
+}
