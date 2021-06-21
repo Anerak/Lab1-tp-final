13b40b84012c88c2501d7f726d26bf3a1710239e
Guntergs <santii001@hotmail.com>
2021-06-21 06:58:38 -0300

    funciones ventas

--- pedidos.c
+++ pedidos.c
@@ -2,7 +2,9 @@
 
 void mostrarPedido(Pedido pedido)
 {
+    system("cls");
     float suma = 0;
+    printf("-----------------------------------------");
     printf("\nPedido %d \n", pedido.id);
     for (int i = 0; i < pedido.cantItems; i++)
     {
@@ -10,9 +12,12 @@ void mostrarPedido(Pedido pedido)
         printf("%s %s - %.2f\n", pedido.items[i].nombre, pedido.items[i].detalles, pedido.items[i].precio);
     }
     printf("Total: %.2f\n", suma);
+    printf("-----------------------------------------\n");
+    system("pause");
 }
 
-void borrarPedido(Pedido *pedido) {
+void borrarPedido(Pedido *pedido)
+{
     pedido->id = 0;
     pedido->activo = 0;
     pedido->cantItems = 0;
